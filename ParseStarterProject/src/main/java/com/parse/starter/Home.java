0ea89f4e5b4856b301c634d50f69b218e6ddc8cf
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-01-24 23:16:15 +1100

    Added logout and projects activity

--- ParseStarterProject/src/main/java/com/parse/starter/Home.java
+++ ParseStarterProject/src/main/java/com/parse/starter/Home.java
@@ -1,7 +1,11 @@
 package com.parse.starter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.parse.Parse;
@@ -19,5 +23,30 @@ public class Home extends AppCompatActivity {
 
         TextView tvWelcome = (TextView) findViewById(R.id.tvWelcome);
         tvWelcome.setText(ParseUser.getCurrentUser().get("name").toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.home_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.projects) {
+            Intent projectsIntent = new Intent(getApplicationContext(), Projects.class);
+            startActivity(projectsIntent);
+        }
+        else if (item.getItemId() == R.id.logout) {
+            ParseUser.logOut();
+            Intent loginIntent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(loginIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
